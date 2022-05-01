ad65abcae09a7959047b04675542cdc6009e2667
ApolloMix <delgado_theon@yahoo.com>
2022-05-01 03:05:16 -0400

    Initialize repo, created database and insert method

---
+++ ToDoListMVC/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ToDoListMVC.Models;
+using Microsoft.Data.Sqlite;
+
+namespace ToDoListMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly IConfiguration _configuration;
+   
+
+    public HomeController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public RedirectResult Insert(ToDoModel todo)
+    {
+        using (SqliteConnection connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
+        {
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText = $@"INSERT INTO ToDo (Name)
+                                    VALUES ('{todo.Name}');";
+            command.ExecuteNonQuery();
+        }
+        return Redirect("https://localhost:7017");
+    }
+}
